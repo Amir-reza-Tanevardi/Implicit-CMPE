7bcc6419614d6de0c39944759b48f0d9ce1febec
Amir-reza-Tanevardi <110600515+Amir-reza-Tanevardi@users.noreply.github.com>
2025-07-24 12:58:33 +0330

    Update train.py

--- experiments/denoising_imagenet_2/train.py
+++ experiments/denoising_imagenet_2/train.py
@@ -7,6 +7,7 @@ import sys
 
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
 from bayesflow.experimental.rectifiers import RectifiedDistribution
 from bayesflow.trainers import Trainer
 from scipy.ndimage import gaussian_filter
