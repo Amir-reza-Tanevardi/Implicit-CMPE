a2461f00b394cd83841d7b1dd5132e00593d131a
Amir-reza-Tanevardi <110600515+Amir-reza-Tanevardi@users.noreply.github.com>
2025-07-24 14:00:45 +0330

    Update train.py

--- experiments/denoising_imagenet_2/train.py
+++ experiments/denoising_imagenet_2/train.py
@@ -338,7 +338,8 @@ class UNetModel(keras.Model):
         self.out_act  = layers.Activation('swish')
         self.out = zero_conv(ch, out_channels)
 
-    def call(self, x, cond_input, timesteps):
+    def call(self, inputs):
+        x, cond_input, timesteps = inputs
         # x: [B,H,W,C], timesteps: [B], cond_input: [B,cond_dim]
         # embed time + cond
         emb = self.time_embed(timesteps)                 # [B, Tdim]
